eae0e27f28059e5f0d71da07ff80d53200971fc9
Trung Pham <pbtrung@outlook.com>
2024-08-01 21:03:16 +0100

    Update

--- src/main.c
+++ src/main.c
@@ -57,7 +57,7 @@ int main(int argc, char *argv[]) {
             }
         }
 
-        printf("\nend");
+        printf("\nend-5z2ok9v4iik5tdykgms90qrc6");
         fflush(stdout);
         download_cleanup(infos, config.num_files);
     }
