27556a62a66960d04960847c20e03a47811f1093
Trung Pham <pbtrung@outlook.com>
2025-08-11 20:15:24 +0700

    Use thread to download files

--- src/main.c
+++ src/main.c
@@ -8,6 +8,14 @@
 #include "dir.h"
 #include "download.h"
 #include "log.h"
+#include "queue.h"
+
+typedef struct {
+    file_queue_t *queue;
+    apr_pool_t *pool;
+    const char *config_file;
+    config_t *config;
+} downloader_args_t;
 
 void initialize_pool(apr_pool_t **pool) {
     if (apr_initialize() != APR_SUCCESS) {
@@ -38,27 +46,34 @@ FILE *setup_logging(const char *config_file, config_t **config) {
     return fp;
 }
 
-void process_files(apr_pool_t *pool, const char *config_file,
-                   config_t **config) {
-    apr_pool_t *subp1;
-    apr_pool_create(&subp1, pool);
+static void *APR_THREAD_FUNC downloader_thread(apr_thread_t *thd, void *data) {
+    (void)thd;
+    downloader_args_t *args = (downloader_args_t *)data;
+    file_queue_t *q = args->queue;
+    apr_pool_t *pool = args->pool;
+    config_t *config = args->config;
+    const char *config_file = args->config_file;
 
-    *config = apr_palloc(subp1, sizeof(config_t));
-    config_read(config_file, *config);
-    apr_pool_cleanup_register(subp1, *config, config_free,
+    apr_pool_t *subpool;
+    apr_pool_create(&subpool, pool);
+
+    config = apr_palloc(subpool, sizeof(config_t));
+    config_read(config_file, config);
+    apr_pool_cleanup_register(subpool, config, config_free,
                               apr_pool_cleanup_null);
 
     sqlite3 *db;
-    database_open_readonly((*config)->db, &db);
-    apr_pool_cleanup_register(subp1, db, database_close, apr_pool_cleanup_null);
+    database_open_readonly(config->db, &db);
+    apr_pool_cleanup_register(subpool, db, database_close,
+                              apr_pool_cleanup_null);
 
-    (*config)->num_tracks = database_count_tracks(db);
-    dir_delete(subp1, (*config)->output);
-    dir_create(subp1, (*config)->output);
+    config->num_tracks = database_count_tracks(db);
+    dir_delete(subpool, config->output);
+    dir_create(subpool, config->output);
 
-    download_assemble_files(subp1, db, *config);
+    download_assemble_files(subpool, db, config);
 
-    apr_pool_destroy(subp1);
+    apr_pool_destroy(subpool);
 }
 
 int main(int argc, const char *argv[]) {
@@ -73,7 +88,16 @@ int main(int argc, const char *argv[]) {
     config_t *config = NULL;
     FILE *fp = setup_logging(argv[1], &config);
 
-    process_files(pool, argv[1], &config);
+    file_queue_t queue;
+    queue_init(&queue, pool);
+    apr_thread_t *dl_thread = NULL;
+    apr_threadattr_t *dl_attr = NULL;
+    downloader_args_t dl_args = {.queue = &queue,
+                                 .pool = pool,
+                                 .config_file = argv[1],
+                                 .config = config};
+    apr_threadattr_create(&dl_attr, pool);
+    apr_thread_create(&dl_thread, dl_attr, downloader_thread, &dl_args, pool);
 
     fclose(fp);
     apr_pool_destroy(pool);
