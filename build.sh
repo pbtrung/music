e036de4d84c82a3d0ac5cf43255531e89e9d0025
Trung Pham <pbtrung@outlook.com>
2025-08-14 00:22:00 +0700

    Build AWS DynamoDB

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 cmake -S . -B build/release -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
 cmake --build build/release --target music
 
-# cmake -S . -B build/examples -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
-# for target in ex01 ex02 ex03; do
-#     cmake --build build/examples --target "$target"
-# done
+cmake -S . -B build/examples -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
+for target in ex01; do
+    cmake --build build/examples --target "$target"
+done
 
 # Build tests in Debug
 # cmake -S . -B build/tests -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug
