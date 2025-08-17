161fe215176a0fadbe4781b637b6a3531ee785cf
Trung Pham <pbtrung@outlook.com>
2025-08-17 21:57:22 +0700

    Thread-safe logging

--- include/log.h
+++ include/log.h
@@ -1,18 +1,12 @@
-/**
- * Copyright (c) 2020 rxi
- *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the MIT license. See `log.c` for details.
- */
-
 #ifndef LOG_H
 #define LOG_H
 
 #include <stdarg.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include <string.h>
 #include <time.h>
+
+#include <apr_thread_mutex.h>
 
 #define LOG_VERSION "0.1.0"
 
@@ -24,10 +18,9 @@ typedef struct {
     void *udata;
     int line;
     int level;
-} log_Event;
+} log_event;
 
-typedef void (*log_LogFn)(log_Event *ev);
-typedef void (*log_LockFn)(bool lock, void *udata);
+typedef void (*log_fn)(log_event *ev);
 
 enum { LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL };
 
@@ -36,26 +29,22 @@ static inline const char *get_filename(const char *path) {
     return last_slash ? last_slash + 1 : path;
 }
 
-#define log_trace(...)                                                         \
-    log_log(LOG_TRACE, get_filename(__FILE__), __LINE__, __VA_ARGS__)
-#define log_debug(...)                                                         \
-    log_log(LOG_DEBUG, get_filename(__FILE__), __LINE__, __VA_ARGS__)
-#define log_info(...)                                                          \
-    log_log(LOG_INFO, get_filename(__FILE__), __LINE__, __VA_ARGS__)
-#define log_warn(...)                                                          \
-    log_log(LOG_WARN, get_filename(__FILE__), __LINE__, __VA_ARGS__)
-#define log_error(...)                                                         \
-    log_log(LOG_ERROR, get_filename(__FILE__), __LINE__, __VA_ARGS__)
-#define log_fatal(...)                                                         \
-    log_log(LOG_FATAL, get_filename(__FILE__), __LINE__, __VA_ARGS__)
+#define log_trace(...) log_log(LOG_TRACE, get_filename(__FILE__), __LINE__, __VA_ARGS__)
+#define log_debug(...) log_log(LOG_DEBUG, get_filename(__FILE__), __LINE__, __VA_ARGS__)
+#define log_info(...)  log_log(LOG_INFO,  get_filename(__FILE__), __LINE__, __VA_ARGS__)
+#define log_warn(...)  log_log(LOG_WARN,  get_filename(__FILE__), __LINE__, __VA_ARGS__)
+#define log_error(...) log_log(LOG_ERROR, get_filename(__FILE__), __LINE__, __VA_ARGS__)
+#define log_fatal(...) log_log(LOG_FATAL, get_filename(__FILE__), __LINE__, __VA_ARGS__)
 
 const char *log_level_string(int level);
-void log_set_lock(log_LockFn fn, void *udata);
 void log_set_level(int level);
 void log_set_quiet(bool enable);
-int log_add_callback(log_LogFn fn, void *udata, int level);
+int log_add_callback(log_fn fn, void *udata, int level);
 int log_add_fp(FILE *fp, int level);
 
 void log_log(int level, const char *file, int line, const char *fmt, ...);
 
-#endif
+int log_init(apr_pool_t *pool);
+void log_shutdown(void);
+
+#endif // LOG_H
--- src/log.c
+++ src/log.c
@@ -1,96 +1,62 @@
-/*
- * Copyright (c) 2020 rxi
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to
- * deal in the Software without restriction, including without limitation the
- * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
- * sell copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
- * IN THE SOFTWARE.
- */
+#include <apr_time.h>
+#include <apr_pools.h>
 
 #include "log.h"
 
 #define MAX_CALLBACKS 32
 
 typedef struct {
-    log_LogFn fn;
+    log_fn fn;
     void *udata;
     int level;
-} Callback;
+} callback;
 
 static struct {
-    void *udata;
-    log_LockFn lock;
     int level;
     bool quiet;
-    Callback callbacks[MAX_CALLBACKS];
+    callback callbacks[MAX_CALLBACKS];
+    apr_thread_mutex_t *mutex;
+    apr_pool_t *pool;
 } L;
 
-static const char *level_strings[] = {"TRACE", "DEBUG", "INFO",
-                                      "WARN",  "ERROR", "FATAL"};
+static const char *level_strings[] = {
+    "TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL"
+};
 
-#ifdef LOG_USE_COLOR
-static const char *level_colors[] = {"\x1b[94m", "\x1b[36m", "\x1b[32m",
-                                     "\x1b[33m", "\x1b[31m", "\x1b[35m"};
-#endif
-
-static void stdout_callback(log_Event *ev) {
+static void stdout_callback(log_event *ev) {
     char buf[16];
     buf[strftime(buf, sizeof(buf), "%H:%M:%S", ev->time)] = '\0';
-#ifdef LOG_USE_COLOR
-    fprintf(ev->udata, "%s %s%-5s\x1b[0m \x1b[90m%s:%d:\x1b[0m ", buf,
-            level_colors[ev->level], level_strings[ev->level], ev->file,
-            ev->line);
-#else
-    fprintf(ev->udata, "%s %-5s %s:%d: ", buf, level_strings[ev->level],
-            ev->file, ev->line);
-#endif
+    fprintf(ev->udata, "%s %-5s %s:%d: ", buf,
+            level_strings[ev->level], ev->file, ev->line);
     vfprintf(ev->udata, ev->fmt, ev->ap);
     fprintf(ev->udata, "\n");
     fflush(ev->udata);
 }
 
-static void file_callback(log_Event *ev) {
+static void file_callback(log_event *ev) {
     char buf[64];
     buf[strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", ev->time)] = '\0';
-    fprintf(ev->udata, "%s %-5s %s:%d: ", buf, level_strings[ev->level],
-            ev->file, ev->line);
+    fprintf(ev->udata, "%s %-5s %s:%d: ",
+            buf, level_strings[ev->level], ev->file, ev->line);
     vfprintf(ev->udata, ev->fmt, ev->ap);
     fprintf(ev->udata, "\n");
     fflush(ev->udata);
 }
 
 static void lock(void) {
-    if (L.lock) {
-        L.lock(true, L.udata);
+    if (L.mutex) {
+        apr_thread_mutex_lock(L.mutex);
     }
 }
 
 static void unlock(void) {
-    if (L.lock) {
-        L.lock(false, L.udata);
+    if (L.mutex) {
+        apr_thread_mutex_unlock(L.mutex);
     }
 }
 
 const char *log_level_string(int level) {
     return level_strings[level];
-}
-
-void log_set_lock(log_LockFn fn, void *udata) {
-    L.lock = fn;
-    L.udata = udata;
 }
 
 void log_set_level(int level) {
@@ -101,10 +67,10 @@ void log_set_quiet(bool enable) {
     L.quiet = enable;
 }
 
-int log_add_callback(log_LogFn fn, void *udata, int level) {
+int log_add_callback(log_fn fn, void *udata, int level) {
     for (int i = 0; i < MAX_CALLBACKS; i++) {
         if (!L.callbacks[i].fn) {
-            L.callbacks[i] = (Callback){fn, udata, level};
+            L.callbacks[i] = (callback){fn, udata, level};
             return 0;
         }
     }
@@ -115,7 +81,7 @@ int log_add_fp(FILE *fp, int level) {
     return log_add_callback(file_callback, fp, level);
 }
 
-static void init_event(log_Event *ev, void *udata) {
+static void init_event(log_event *ev, void *udata) {
     if (!ev->time) {
         time_t t = time(NULL);
         ev->time = localtime(&t);
@@ -124,7 +90,7 @@ static void init_event(log_Event *ev, void *udata) {
 }
 
 void log_log(int level, const char *file, int line, const char *fmt, ...) {
-    log_Event ev = {
+    log_event ev = {
         .fmt = fmt,
         .file = file,
         .line = line,
@@ -141,7 +107,7 @@ void log_log(int level, const char *file, int line, const char *fmt, ...) {
     }
 
     for (int i = 0; i < MAX_CALLBACKS && L.callbacks[i].fn; i++) {
-        Callback *cb = &L.callbacks[i];
+        callback *cb = &L.callbacks[i];
         if (level >= cb->level) {
             init_event(&ev, cb->udata);
             va_start(ev.ap, fmt);
@@ -151,4 +117,24 @@ void log_log(int level, const char *file, int line, const char *fmt, ...) {
     }
 
     unlock();
+}
+
+int log_init(apr_pool_t *pool) {
+    L.pool = pool;
+    L.level = LOG_TRACE;
+    L.quiet = false;
+    for (int i = 0; i < MAX_CALLBACKS; i++) {
+        L.callbacks[i].fn = NULL;
+    }
+    if (apr_thread_mutex_create(&L.mutex, APR_THREAD_MUTEX_DEFAULT, pool) != APR_SUCCESS) {
+        return -1;
+    }
+    return 0;
+}
+
+void log_shutdown(void) {
+    if (L.mutex) {
+        apr_thread_mutex_destroy(L.mutex);
+        L.mutex = NULL;
+    }
 }
