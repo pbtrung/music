1dae7388e35f16d20ad1d30047b0394560ec9e2b
Trung Pham <trungpb2@vmogroup.com>
2025-08-18 11:40:45 +0700

    Update build and example

--- examples/ex01.c
+++ examples/ex01.c
@@ -5,33 +5,6 @@
 #include "cosmosdb.h"
 #include "download.h"
 
-static void print_file_info(const file_info_t *info) {
-    if (!info) {
-        printf("(null file_info_t)\n");
-        return;
-    }
-
-    printf("=== file_info_t ===\n");
-    printf("Filename: %s\n", info->filename ? info->filename : "(null)");
-    printf("Track Name: %s\n", info->track_name ? info->track_name : "(null)");
-    printf("Extension: %s\n", info->extension ? info->extension : "(null)");
-    printf("Album Path: %s\n", info->album_path ? info->album_path : "(null)");
-    printf("Track ID: %d\n", info->track_id);
-
-    printf("Number of CIDs: %d\n", info->num_cids);
-    if (info->cids) {
-        for (int i = 0; i < info->num_cids; i++) {
-            printf("  CID[%d]: %s\n", i, info->cids[i] ? info->cids[i] : "(null)");
-            if (info->cid_download_status) {
-                printf("    CID Status: %d\n", info->cid_download_status[i]);
-            }
-        }
-    }
-
-    printf("File Download Status: %d\n", info->file_download_status);
-    printf("Config pointer: %p\n", (void *)info->config);
-}
-
 static void exit_on_error(const char *msg) {
     fprintf(stderr, "%s\n", msg);
     exit(-1);
@@ -71,12 +44,6 @@ int main(int argc, const char *argv[]) {
         printf("%s\n", dump);
         free(dump);
     }
-
-    file_info_t *info = apr_palloc(pool, sizeof(file_info_t));
-    apr_pool_cleanup_register(pool, info, file_info_free,
-                              apr_pool_cleanup_null);
-    cosmosdb_file_info_init(info, doc, cfg);
-    // print_file_info(info);
 
     json_decref(doc);
     apr_pool_destroy(pool);
