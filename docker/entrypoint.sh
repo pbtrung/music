4ee36a25dbd57b40f37136818d024e875357101c
Trung Pham <pbtrung@outlook.com>
2025-08-23 12:11:47 +0700

    Make sure to kill gwsocket

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,15 @@
 #!/bin/ash
 
 snapserver --config "$1" &
-music "$2" | gwsocket --port=9000 --addr=0.0.0.0 --std
+
+mkfifo /tmp/music_pipe
+music "$2" > /tmp/music_pipe &
+music_pid=$!
+gwsocket --port=9000 --addr=0.0.0.0 --std < /tmp/music_pipe &
+gwsocket_pid=$!
+
+# Wait for music to exit
+wait $music_pid
+# Kill gwsocket when music exits
+kill -TERM $gwsocket_pid 2>/dev/null
+rm /tmp/music_pipe
