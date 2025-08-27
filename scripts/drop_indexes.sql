e5201b3d8cd0f703f0bf82b64fe075153a5bbdbb
Trung Pham <pbtrung@outlook.com>
2025-08-27 20:49:57 +0700

    Add drop indexes sql statements

---
+++ scripts/drop_indexes.sql
@@ -0,0 +1,6 @@
+DROP INDEX IF EXISTS idx_album_path;
+DROP INDEX IF EXISTS idx_track_name;
+DROP INDEX IF EXISTS idx_cid;
+DROP INDEX IF EXISTS idx_gdr_cid;
+DROP INDEX IF EXISTS idx_gdr_email;
+VACUUM;
