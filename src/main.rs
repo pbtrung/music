403cfa179a504ea8630bc34975c851ea68a6d3b0
Trung Pham <pbtrung@outlook.com>
2025-09-06 09:47:47 +0700

    Clean up main

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,8 @@ mod config;
 use config::Config;
 
 mod audio_decoder;
-mod migrate;
 mod track;
 mod utils;
-
-mod r2duckdb;
-use r2duckdb::R2DuckDB;
 
 #[derive(Parser, Debug)]
 #[command(name = "music")]
@@ -70,30 +66,12 @@ async fn main() -> Result<()> {
     match args.command {
         Some(Commands::Migrate) => {
             log::info!(
-                "Running migrations using config: {:?}",
+                "Running migration using config: {:?}",
                 &args.config.display()
             );
-            migrate::run_migrate(
-                &config.sqlitedb,
-                &config.duckdb,
-                &config.parquet,
-                config.min_value,
-            )?;
         }
         Some(Commands::Run) | None => {
             log::info!("Running app with config: {:?}", &args.config.display());
-            let r2_db = R2DuckDB::new(config.r2);
-            let rand_track_id = utils::generate_unique_ints(1, config.min_value, config.max_value)
-                .ok_or(anyhow::anyhow!("Failed to generate random track ID"))?
-                .pop()
-                .unwrap();
-            let track = r2_db.get_track(rand_track_id as u64)?;
-            if let Some(track) = track {
-                let json = serde_json::to_string_pretty(&track)?;
-                println!("{}", json);
-            } else {
-                println!("No track found with ID: {}", rand_track_id);
-            }
         }
     }
 
