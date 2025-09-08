4090fe725a0fb4613a7e4ee06aa6b566cc220cce
Trung Pham <pbtrung@outlook.com>
2025-09-08 19:15:33 +0700

    Disable ex02 to deploy

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@
 cmake -S . -B build/release -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
 cmake --build build/release --target music
 
-example_list=(
-    ex02
-)
-cmake -S . -B build/examples -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
-for target in "${example_list[@]}"; do
-    cmake --build build/examples --target "$target"
-done
+# example_list=(
+#     ex02
+# )
+# cmake -S . -B build/examples -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
+# for target in "${example_list[@]}"; do
+#     cmake --build build/examples --target "$target"
+# done
 
 # Build tests in Debug
 # cmake -S . -B build/tests -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug
